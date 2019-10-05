a9ea4a3d0db9ec166d5e9e51719f68dd8c2f1849
xinggevip <1511844263@qq.com>
2019-10-06 00:15:03 +0800

    Hibernate插入更新获取操作

--- HibernateProject/src/com/gaoxing/test/HibernateTest.java
+++ HibernateProject/src/com/gaoxing/test/HibernateTest.java
@@ -3,11 +3,14 @@ package com.gaoxing.test;
 import com.gaoxing.domain.Customer;
 import com.gaoxing.utils.HibernateUtils;
 import org.hibernate.Session;
+import org.hibernate.Transaction;
+import org.junit.Test;
 
 public class HibernateTest {
 
-
-    static void test1(){
+    // 插入记录
+    @Test
+    public void test1(){
         Session session = HibernateUtils.openSession();
 
         // 要操作谁就创建谁
@@ -21,7 +24,99 @@ public class HibernateTest {
         HibernateUtils.sessionFactory.close();
     }
 
-    public static void main(String[] args){
-        test1();
+    // 查询记录，根据id
+    @Test
+    public void test2(){
+        // 获取连接对象
+        Session session = HibernateUtils.openSession();
+        // 查询一条纪录
+        Customer customer = session.get(Customer.class, 34L);
+        // 打印
+        System.out.println(customer);
+        // 释放资源
+        session.close();
+    }
+
+    // 更新记录一
+    @Test
+    public void test3(){
+        /**
+         * 更新操作方法一
+         * 创建对象更新，只更新设置的字段，没有设置的字段自动设置为null
+         */
+
+        // 获取连接对象
+        Session session = HibernateUtils.openSession();
+        // 更新操作必须开启事务
+        Transaction transaction = session.beginTransaction();
+        // 创建更新对象
+        Customer customer = new Customer();
+        customer.setCust_id(34L);
+        customer.setCust_name("星哥无敌");
+        // 更新
+        session.update(customer);
+        // 提交事务
+        transaction.commit();
+        // 释放资源
+        session.close();
+    }
+
+    // 更新记录2
+    @Test
+    public void test4(){
+        /**
+         * 更新操作方法二
+         * 先查询再更新，只更新设置的字段，效果理想
+         */
+
+        // 获取连接对象
+        Session session = HibernateUtils.openSession();
+        // 更新操作必须开启事务
+        Transaction transaction = session.beginTransaction();
+        Customer customer = session.get(Customer.class, 34L);
+        customer.setCust_name("修改后的名字");
+        customer.setCust_level("5");
+        // 更新
+        session.update(customer);
+        // 提交事务
+        transaction.commit();
+        // 释放资源
+        session.close();
+    }
+
+    // 删除记录一
+    @Test
+    public void test5(){
+        // 获取连接对象
+        Session session = HibernateUtils.openSession();
+        // 开启事务
+        Transaction transaction = session.beginTransaction();
+        // 创建对象
+        Customer customer = new Customer();
+        // 设置对象id属性
+        customer.setCust_id(35L);
+        // 根据对象id属性删除记录
+        session.delete(customer);
+        // 提交事务
+        transaction.commit();
+        // 关闭资源
+        session.close();
+    }
+
+    // 删除记录二
+    @Test
+    public void test6(){
+        // 获取连接对象
+        Session session = HibernateUtils.openSession();
+        // 开启事务
+        Transaction transaction = session.beginTransaction();
+        // 获取对象
+        Customer customer = session.get(Customer.class, 36L);
+        // 根据对象id属性删除记录
+        session.delete(customer);
+        // 提交事务
+        transaction.commit();
+        // 关闭资源
+        session.close();
     }
 }
