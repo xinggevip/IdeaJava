c1741a6977106fd36b7dc21249e1fe095638d09e
xinggevip <1511844263@qq.com>
2019-10-06 21:52:24 +0800

    Hibernate级联保存更新操作

--- HibernateProject2/src/com/gaoxing/test/HibernateTest.java
+++ HibernateProject2/src/com/gaoxing/test/HibernateTest.java
@@ -61,4 +61,37 @@ public class HibernateTest {
         System.out.println(linkman.getLink_name());
         System.out.println(linkman.getCustomer().getCust_name());
     }
+
+    // 级联保存
+    @Test
+    public void test3(){
+        Session currentSession = HibernateUtils.getCurrentSession();
+        Transaction transaction = currentSession.beginTransaction();
+
+        Customer customer1 = new Customer();
+        customer1.setCust_name("customer1");
+        Customer customer2 = new Customer();
+        customer2.setCust_name("customer2");
+        Customer customer3 = new Customer();
+        customer3.setCust_name("customer3");
+
+        Linkman linkman1 = new Linkman();
+        linkman1.setLink_name("linkman1");
+        Linkman linkman2 = new Linkman();
+        linkman2.setLink_name("linkman2");
+        Linkman linkman3 = new Linkman();
+        linkman3.setLink_name("linkman3");
+
+        // 配置关系
+        customer1.getLinkmens().add(linkman1);
+        customer1.getLinkmens().add(linkman2);
+        customer2.getLinkmens().add(linkman3);
+
+        // 保存
+        currentSession.save(customer1);
+        currentSession.save(customer2);
+        currentSession.save(customer3);
+
+        transaction.commit();
+    }
 }
