d5f50495eae2fe562aac8eaf7167943324168290
xinggevip <1511844263@qq.com>
2019-10-09 21:57:31 +0800

    Struts2接收参数

---
+++ StrutsPro2/src/com/gaoxing/myform/MyformAction.java
@@ -0,0 +1,26 @@
+package com.gaoxing.myform;
+
+import com.opensymphony.xwork2.ActionContext;
+import com.opensymphony.xwork2.ActionSupport;
+import org.apache.struts2.dispatcher.HttpParameters;
+
+import java.util.Arrays;
+
+public class MyformAction extends ActionSupport {
+    public String execute(){
+        // 接收表单中的参数
+        ActionContext context = ActionContext.getContext();
+        HttpParameters parameters = context.getParameters();
+        // 获取参数
+        String username = parameters.get("username").getValue();
+        String nick = parameters.get("nick").getValue();
+        String[] hobbies = parameters.get("hobby").getMultipleValues();
+
+        System.out.println(username);
+        System.out.println(nick);
+        System.out.println(Arrays.toString(hobbies));
+
+        System.out.println("接收到表单");
+        return null;
+    }
+}
---
+++ StrutsPro2/web/index.jsp
@@ -0,0 +1,27 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: xingge
+  Date: 2019/10/9
+  Time: 21:15
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" pageEncoding="utf-8" %>
+<%
+  String ctx = request.getContextPath();
+  pageContext.setAttribute("ctx", ctx);
+%>
+<html>
+  <head>
+    <title>$Title$</title>
+  </head>
+  <body>
+  <form action="${ctx}/myform.action">
+    用户名：<input type="text" placeholder="请输入用户名..." name="username"><br/>
+    昵 称：<input type="text" placeholder="请输入爱好..." name="nick"><br/>
+    爱 好：<input type="checkbox"  value="足球" name="hobby">足球
+    <input type="checkbox"  value="篮球" name="hobby">篮球
+    <input type="checkbox"  value="乒乓球" name="hobby">乒乓球
+    <input type="submit" value="提交">
+  </form>
+  </body>
+</html>
