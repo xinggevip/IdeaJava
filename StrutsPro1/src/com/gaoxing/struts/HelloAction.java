634fa4250c41103ac433c57d6b9479c05b7d85fd
xinggevip <1511844263@qq.com>
2019-10-09 16:19:43 +0800

    Struts2返回结果跳转

--- StrutsPro1/src/com/gaoxing/struts/HelloAction.java
+++ StrutsPro1/src/com/gaoxing/struts/HelloAction.java
@@ -3,6 +3,6 @@ package com.gaoxing.struts;
 public class HelloAction {
     public String execute(){
         System.out.println("接收到了请求！");
-        return null;
+        return "success";
     }
 }
