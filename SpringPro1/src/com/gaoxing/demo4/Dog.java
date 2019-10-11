2402d52f5ffcc627e4350e74e0eb492fa10dc337
xinggevip <1511844263@qq.com>
2019-10-11 15:18:38 +0800

    Spring-引用类型属性注入

---
+++ SpringPro1/src/com/gaoxing/demo4/Dog.java
@@ -0,0 +1,16 @@
+package com.gaoxing.demo4;
+
+public class Dog {
+    private String name;
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Dog{" +
+                "name='" + name + '\'' +
+                '}';
+    }
+}
--- SpringPro1/src/com/gaoxing/demo4/Student.java
+++ SpringPro1/src/com/gaoxing/demo4/Student.java
@@ -3,6 +3,11 @@ package com.gaoxing.demo4;
 public class Student {
     private String name;
     private Integer age;
+    private Dog dog;
+
+    public void setDog(Dog dog) {
+        this.dog = dog;
+    }
 
     public Student(String name, Integer age) {
         this.name = name;
@@ -14,6 +19,7 @@ public class Student {
         return "Student{" +
                 "name='" + name + '\'' +
                 ", age=" + age +
+                ", dog=" + dog +
                 '}';
     }
 }
