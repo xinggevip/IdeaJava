601010301ffc006470e16eee79d946471ed614de
xinggevip <1511844263@qq.com>
2019-10-11 15:29:10 +0800

    Spring-p名称空间属性注入

--- SpringPro1/src/com/gaoxing/demo4/Dog.java
+++ SpringPro1/src/com/gaoxing/demo4/Dog.java
@@ -2,6 +2,16 @@ package com.gaoxing.demo4;
 
 public class Dog {
     private String name;
+    private Integer age;
+    private String color;
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
 
     public void setName(String name) {
         this.name = name;
@@ -11,6 +21,8 @@ public class Dog {
     public String toString() {
         return "Dog{" +
                 "name='" + name + '\'' +
+                ", age=" + age +
+                ", color='" + color + '\'' +
                 '}';
     }
 }
--- SpringPro1/src/com/gaoxing/demo4/StudentTest.java
+++ SpringPro1/src/com/gaoxing/demo4/StudentTest.java
@@ -10,6 +10,6 @@ public class StudentTest {
         ApplicationContext applicationContext =
                 new ClassPathXmlApplicationContext("applicationContext.xml");
         Student student = (Student)applicationContext.getBean("student");
-        System.out.println(student); // Student{name='jeak', age=18, dog=Dog{name='wc'}}
+        System.out.println(student); // Student{name='jeak', age=18, dog=Dog{name='大黄', age=10, color='yellow'}}
     }
 }
