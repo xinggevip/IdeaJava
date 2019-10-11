b30a9d00761b76a97fa53871e76b73fbbdf3541b
xinggevip <1511844263@qq.com>
2019-10-11 16:21:26 +0800

    Spring-集合类型属性注入

--- SpringPro1/src/com/gaoxing/demo4/Student.java
+++ SpringPro1/src/com/gaoxing/demo4/Student.java
@@ -1,10 +1,40 @@
 package com.gaoxing.demo4;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
 public class Student {
     private String name;
     private Integer age;
     private Dog dog;
     private String dogName;
+    /* 集合类型 */
+    // 字符串数组
+    private String[] attr;
+    // list集合
+    private List list;
+    // set集合
+    private Set mySet;
+    // map集合
+    private Map myMap;
+
+    public void setMyMap(Map myMap) {
+        this.myMap = myMap;
+    }
+
+    public void setMySet(Set mySet) {
+        this.mySet = mySet;
+    }
+
+    public void setList(List list) {
+        this.list = list;
+    }
+
+    public void setAttr(String[] attr) {
+        this.attr = attr;
+    }
 
     public void setDogName(String dogName) {
         this.dogName = dogName;
@@ -26,6 +56,10 @@ public class Student {
                 ", age=" + age +
                 ", dog=" + dog +
                 ", dogName='" + dogName + '\'' +
+                ", attr=" + Arrays.toString(attr) +
+                ", list=" + list +
+                ", mySet=" + mySet +
+                ", myMap=" + myMap +
                 '}';
     }
 }
--- SpringPro1/src/com/gaoxing/demo4/StudentTest.java
+++ SpringPro1/src/com/gaoxing/demo4/StudentTest.java
@@ -10,6 +10,7 @@ public class StudentTest {
         ApplicationContext applicationContext =
                 new ClassPathXmlApplicationContext("applicationContext.xml");
         Student student = (Student)applicationContext.getBean("student");
-        System.out.println(student); // Student{name='jeak', age=18, dog=Dog{name='wccc', age=180, color='blue'}, dogName='wccc'}
+        System.out.println(student);
+        // Student{name='jeak', age=18, dog=Dog{name='wccc', age=180, color='blue'}, dogName='wccc', attr=[H, e, l, l, o], list=[今天, 天气, 不错], mySet=[这里是, set, 集合], myMap={key1=value1, key2=value2, key3=value3}}
     }
 }
