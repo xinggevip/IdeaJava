477f2e7de62b3ea8f1924ec94d54f9a88c2a8935
xinggevip <1511844263@qq.com>
2019-10-14 17:51:33 +0800

    Spring-spring-test使用

---
+++ SpringPro4/src/com/gaoxing/demo2/AopTest.java
@@ -0,0 +1,20 @@
+package com.gaoxing.demo2;
+
+import com.gaoxing.demo1.GoodsDao;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import javax.annotation.Resource;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("classpath:applicationContext.xml")
+public class AopTest {
+    @Resource(name = "userDao")
+    private GoodsDao goodsDao;
+    @Test
+    public void test1(){
+        this.goodsDao.save();
+    }
+}
