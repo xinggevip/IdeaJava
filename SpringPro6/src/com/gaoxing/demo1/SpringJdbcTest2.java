46027f7663c3d207cbad2bd8f393f0031fdc545f
xinggevip <1511844263@qq.com>
2019-10-15 22:56:07 +0800

    Spring-JDBC模板CRUD操作

--- SpringPro6/src/com/gaoxing/demo1/SpringJdbcTest2.java
+++ SpringPro6/src/com/gaoxing/demo1/SpringJdbcTest2.java
@@ -14,8 +14,35 @@ public class SpringJdbcTest2 {
 
     @Resource(name = "jdbcTemplate")
     private JdbcTemplate jdbcTemplate;
+    // 插入
     @Test
     public void test1(){
         jdbcTemplate.update("insert into account values (null,?,?)","高星3",999d);
+    }
+
+    // 删除
+    @Test
+    public void test2(){
+        jdbcTemplate.update("delete from account where id=?",2);
+    }
+
+    // 更新
+    @Test
+    public void test3(){
+        jdbcTemplate.update("update account set name=?,money=? where id=?","油条",666d,1);
+    }
+
+    // 根据id查询记录
+    @Test
+    public void test4(){
+        String name = jdbcTemplate.queryForObject("select name from account where id=?", String.class, 1);
+        System.out.println(name); // 油条
+    }
+
+    // 查询总计
+    @Test
+    public void test5(){
+        Long count = jdbcTemplate.queryForObject("select count(*) from account", Long.class);
+        System.out.println(count); // 4
     }
 }
