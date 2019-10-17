8a7c79ef9abcbdcd0a5ca30db3e248ed77ba3092
xinggevip <1511844263@qq.com>
2019-10-17 15:11:13 +0800

    SSH框架整合-自动注入Spring对象

--- SSHProject1/src/com/gaoxing/web/AccountAction.java
+++ SSHProject1/src/com/gaoxing/web/AccountAction.java
@@ -4,11 +4,6 @@ import com.gaoxing.domain.Account;
 import com.gaoxing.service.AccountService;
 import com.opensymphony.xwork2.ActionSupport;
 import com.opensymphony.xwork2.ModelDriven;
-import org.apache.struts2.ServletActionContext;
-import org.springframework.web.context.WebApplicationContext;
-import org.springframework.web.context.support.WebApplicationContextUtils;
-
-import javax.servlet.ServletContext;
 
 public class AccountAction extends ActionSupport implements ModelDriven<Account> {
     private Account account = new Account();
@@ -16,15 +11,24 @@ public class AccountAction extends ActionSupport implements ModelDriven<Account>
     public Account getModel() {
         return account;
     }
+
+    // 不用再通过spring获取对象，通过struts2-spring-插件自动注入获取对象
+    AccountService accountService;
+
+    public void setAccountService(AccountService accountService) {
+        this.accountService = accountService;
+    }
+
     public String save(){
         /*System.out.println("AccountAction-----save");
         System.out.println(account);*/
 
-        // 通过spring获取对象
+        /*// 通过spring获取对象
         ServletContext servletContext = ServletActionContext.getServletContext();
         WebApplicationContext applicationContext = WebApplicationContextUtils.getWebApplicationContext(servletContext);
         // 获取对象
         AccountService accountService = (AccountService)applicationContext.getBean("accountService");
+        accountService.save(account);*/
         accountService.save(account);
         return null;
     }
