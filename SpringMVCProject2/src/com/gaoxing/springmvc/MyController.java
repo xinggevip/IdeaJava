66e97378f33790e83222cdb67cd9c91f52a932a4
xinggevip <1511844263@qq.com>
2019-10-18 14:38:02 +0800

    SpringMVC-redirect重定向

--- SpringMVCProject2/src/com/gaoxing/springmvc/MyController.java
+++ SpringMVCProject2/src/com/gaoxing/springmvc/MyController.java
@@ -7,20 +7,22 @@ import org.springframework.web.servlet.ModelAndView;
 @Controller
 public class MyController {
 
-    @RequestMapping("/first.action")
+    // 匹配请求可以省略.action
+    @RequestMapping("/first")
     public ModelAndView show(){
         ModelAndView modelAndView = new ModelAndView();
 
         // 请求过来后，跳转到另一个界面，往另一个界面传一些数据
         modelAndView.addObject("name","gaoxing");
 
-         modelAndView.setViewName("/result.jsp");
+         modelAndView.setViewName("redirect:/result.jsp");
+         // modelAndView.setViewName("redirect:/second.action"); // // 发送请求不可以省略.action
 
         return modelAndView;
     }
 
-    @RequestMapping("/second.action")
+    @RequestMapping("/second")
     public String show2(){
-        return "/result.jsp";
+        return "redirect:/result.jsp";
     }
 }
