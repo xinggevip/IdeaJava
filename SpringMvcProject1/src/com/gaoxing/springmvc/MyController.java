38e8784e8e87931db010ad3e0b778510d4a467fd
xinggevip <1511844263@qq.com>
2019-10-18 13:04:21 +0800

    SpringMVC-快速入门程序

---
+++ SpringMvcProject1/src/com/gaoxing/springmvc/MyController.java
@@ -0,0 +1,21 @@
+package com.gaoxing.springmvc;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class MyController {
+
+    @RequestMapping("/first.action")
+    public ModelAndView show(){
+        ModelAndView modelAndView = new ModelAndView();
+
+        // 请求过来后，跳转到另一个界面，往另一个界面传一些数据
+        modelAndView.addObject("name","gaoxing");
+
+        modelAndView.setViewName("/result.jsp");
+
+        return modelAndView;
+    }
+}
---
+++ SpringMvcProject1/web/index.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: xingge
+  Date: 2019/10/18
+  Time: 12:31
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+  <head>
+    <title>$Title$</title>
+  </head>
+  <body>
+  <a href="${pageContext.request.contextPath}/first.action">跳转</a>
+  </body>
+</html>
