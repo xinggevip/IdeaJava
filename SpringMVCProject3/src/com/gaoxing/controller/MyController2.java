638b954144279cef9dd298e3883e1083286e7775
xinggevip <1511844263@qq.com>
2019-10-19 17:13:43 +0800

    SpringMVC-解决post请求中文乱码

--- SpringMVCProject3/src/com/gaoxing/controller/MyController2.java
+++ SpringMVCProject3/src/com/gaoxing/controller/MyController2.java
@@ -10,10 +10,11 @@ import java.util.Date;
 @Controller
 public class MyController2 {
     @RequestMapping("convert")
-    public ModelAndView testconvert(String age, Date date){
+    public ModelAndView testconvert(String age, Date date,String name){
         System.out.println(age); // 18
         System.out.println(date); // Sat Mar 09 00:00:00 GMT+08:00 2019
         System.out.println(new SimpleDateFormat("yyyy-MM-dd").format(date)); // 2019-03-09
+        System.out.println(name);
         ModelAndView modelAndView = new ModelAndView();
         modelAndView.setViewName("/second.jsp");
         return modelAndView;
--- SpringMVCProject3/web/convert.jsp
+++ SpringMVCProject3/web/convert.jsp
@@ -11,9 +11,10 @@
     <title>convert</title>
 </head>
 <body>
-<form action="${pageContext.request.contextPath}/convert">
+<form action="${pageContext.request.contextPath}/convert" method="post">
     年龄：<input type="text" name="age">
     生日：<input type="text" name="date">
+    姓名：<input type="text" name="name">
     <input type="submit" value="提交">
 </form>
 </body>
