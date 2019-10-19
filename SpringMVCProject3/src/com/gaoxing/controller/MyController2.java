f309f0098f0befea4ea90e56b9a8af1f2d8b082f
xinggevip <1511844263@qq.com>
2019-10-19 17:02:00 +0800

    SpringMVC-自定义日期参数类型转换器

--- SpringMVCProject3/src/com/gaoxing/controller/MyController2.java
+++ SpringMVCProject3/src/com/gaoxing/controller/MyController2.java
@@ -4,14 +4,16 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
 @Controller
 public class MyController2 {
     @RequestMapping("convert")
     public ModelAndView testconvert(String age, Date date){
-        System.out.println(age); // 10
+        System.out.println(age); // 18
         System.out.println(date); // Sat Mar 09 00:00:00 GMT+08:00 2019
+        System.out.println(new SimpleDateFormat("yyyy-MM-dd").format(date)); // 2019-03-09
         ModelAndView modelAndView = new ModelAndView();
         modelAndView.setViewName("/second.jsp");
         return modelAndView;
