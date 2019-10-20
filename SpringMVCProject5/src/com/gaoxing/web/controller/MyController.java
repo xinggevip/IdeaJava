faecd2ead97f466289db7f7ed2e9895b262ad447
xinggevip <1511844263@qq.com>
2019-10-20 23:09:20 +0800

    SpringMVC-mvc:view-controller配置-不设置映射直接跳转

--- SpringMVCProject5/src/com/gaoxing/web/controller/MyController.java
+++ SpringMVCProject5/src/com/gaoxing/web/controller/MyController.java
@@ -1,13 +1,8 @@
 package com.gaoxing.web.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class MyController {
-    @RequestMapping("testView")
-    public String testView(){
 
-        return "result.jsp";
-    }
 }
