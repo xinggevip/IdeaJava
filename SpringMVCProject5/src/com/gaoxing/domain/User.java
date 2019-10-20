00cac74d1ede4d5ecc01380a29940e37e2e0d57d
xinggevip <1511844263@qq.com>
2019-10-20 23:03:37 +0800

    SpringMVC-准备文件

---
+++ SpringMVCProject5/src/com/gaoxing/domain/User.java
@@ -0,0 +1,11 @@
+package com.gaoxing.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter @Setter @ToString
+public class User {
+    private String username;
+    private String age;
+}
---
+++ SpringMVCProject5/src/com/gaoxing/web/controller/MyController.java
@@ -0,0 +1,13 @@
+package com.gaoxing.web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MyController {
+    @RequestMapping("testView")
+    public String testView(){
+
+        return "result.jsp";
+    }
+}
