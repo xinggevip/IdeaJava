b54e16fd08eb507b6a587b73a18c4d0dc73a6a08
xinggevip <1511844263@qq.com>
2019-10-23 10:09:29 +0800

    SpringMVC-文件上传

--- SpringMVCProject6/src/com/gaoxing/web/controller/MyController.java
+++ SpringMVCProject6/src/com/gaoxing/web/controller/MyController.java
@@ -6,9 +6,12 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.commons.CommonsMultipartFile;
 
 import javax.servlet.ServletContext;
 import javax.servlet.http.HttpSession;
+import java.io.File;
 import java.io.FileInputStream;
 import java.net.URLEncoder;
 
@@ -40,5 +43,31 @@ public class MyController {
         ResponseEntity<byte[]> responseEntity = new ResponseEntity<>(body, httpHeaders, HttpStatus.OK);
 
         return responseEntity;
+    }
+
+    @RequestMapping("/upload")
+    public String upload(@RequestParam("file")CommonsMultipartFile file,HttpSession session) throws Exception {
+        System.out.println("文件类型：" + file.getContentType());
+        System.out.println("文件名称：" + file.getOriginalFilename());
+        System.out.println("文件大小：" + file.getSize() + "字节");
+        System.out.println("文件属性名称：" + file.getName());
+
+        //确定上传的路径
+        ServletContext servletContext = session.getServletContext();
+        String realPath = servletContext.getRealPath("/upload");
+        //变成程序当中的路径
+        File uploadPath = new File(realPath);
+        if(!uploadPath.exists()){//如果路径不存在 ,创建一个新的
+            uploadPath.mkdirs();
+        }
+        //确认最终的路径  /文件夹/文件名    工程的名称/upload/java.png
+        String fileName = file.getOriginalFilename();
+        uploadPath = new File(uploadPath+"/"+fileName);
+
+        //开始上传
+        file.transferTo(uploadPath);
+
+
+        return "success";
     }
 }
--- SpringMVCProject6/web/index.jsp
+++ SpringMVCProject6/web/index.jsp
@@ -15,7 +15,11 @@
   <hr>
   <a href="${pageContext.request.contextPath}/download/solo.png">下载solo.png</a>
   <a href="${pageContext.request.contextPath}/download/妹子.jpg">下载妹子.jpg</a>
-
+  <hr>
+  <form action="${pageContext.request.contextPath}/upload" method="post" enctype="multipart/form-data">
+    <input type="file" name="file"> <br>
+    <input type="submit" value="上传">
+  </form>
 
   </body>
 </html>
