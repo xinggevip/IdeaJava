2502b8604350d00933db21779d078ef022585b7b
xinggevip <1511844263@qq.com>
2019-10-23 22:46:49 +0800

    SpringMVC-多个拦截器执行顺序2

--- SpringMVCProject6/src/com/gaoxing/web/interceptor/MySecondnterceptor.java
+++ SpringMVCProject6/src/com/gaoxing/web/interceptor/MySecondnterceptor.java
@@ -12,7 +12,7 @@ public class MySecondnterceptor implements HandlerInterceptor {
     // 返回值：true  放行  false 不放行  不放行就执行不了处理器方法
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
         System.out.println("preHandle处理器方法执行前调用2");
-        return true;
+        return false;
     }
 
     // 处理器方法执行后调用
@@ -24,12 +24,9 @@ public class MySecondnterceptor implements HandlerInterceptor {
     public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, @Nullable Exception ex) throws Exception {
         System.out.println("afterCompletion请求处理完毕后调用2");
         /**
-         * 拦截器执行顺序，preHandle按照顺序拦截，postHandle，afterCompletion倒序拦截
+         * 第二个拦截器return false的执行顺序
          * preHandle处理器方法执行前调用1
          * preHandle处理器方法执行前调用2
-         * postHandle处理器方法执行后调用2
-         * postHandle处理器方法执行后调用1
-         * afterCompletion请求处理完毕后调用2
          * afterCompletion请求处理完毕后调用1
          */
     }
