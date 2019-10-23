77bcb03850f4cefa67ef6423da3b56a646c05dd5
xinggevip <1511844263@qq.com>
2019-10-23 22:03:11 +0800

    SpringMVC-多个拦截器执行顺序

--- SpringMVCProject6/src/com/gaoxing/web/interceptor/MySecondnterceptor.java
+++ SpringMVCProject6/src/com/gaoxing/web/interceptor/MySecondnterceptor.java
@@ -7,21 +7,30 @@ import org.springframework.web.servlet.ModelAndView;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-public class MyFirstInterceptor implements HandlerInterceptor {
+public class MySecondnterceptor implements HandlerInterceptor {
     // 处理器方法执行前调用
     // 返回值：true  放行  false 不放行  不放行就执行不了处理器方法
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-        System.out.println("preHandle处理器方法执行前调用1");
+        System.out.println("preHandle处理器方法执行前调用2");
         return true;
     }
 
     // 处理器方法执行后调用
     public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, @Nullable ModelAndView modelAndView) throws Exception {
-        System.out.println("postHandle处理器方法执行后调用1");
+        System.out.println("postHandle处理器方法执行后调用2");
     }
 
     // 请求处理完毕后调用
     public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, @Nullable Exception ex) throws Exception {
-        System.out.println("afterCompletion请求处理完毕后调用1");
+        System.out.println("afterCompletion请求处理完毕后调用2");
+        /**
+         * 拦截器执行顺序，preHandle按照顺序拦截，postHandle，afterCompletion倒序拦截
+         * preHandle处理器方法执行前调用1
+         * preHandle处理器方法执行前调用2
+         * postHandle处理器方法执行后调用2
+         * postHandle处理器方法执行后调用1
+         * afterCompletion请求处理完毕后调用2
+         * afterCompletion请求处理完毕后调用1
+         */
     }
 }
