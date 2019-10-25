6f91eb3d7aafccb9dcd3feb47842a15e9cfa7caf
xinggevip <1511844263@qq.com>
2019-10-25 20:05:11 +0800

    MyBatis-Mapper传参多个普通类型与@param

--- MyBatisProject2/src/com/gaoxing/mapper/CustomerMapper.java
+++ MyBatisProject2/src/com/gaoxing/mapper/CustomerMapper.java
@@ -1,7 +1,23 @@
 package com.gaoxing.mapper;
 
 import com.gaoxing.domain.Customer;
+import org.apache.ibatis.annotations.Param;
+
+/**
+ * 参数传递
+ *
+ * 单个参数
+ * 可以接受基本类型，对象类型，集合类型的值。
+ * MyBatis可直接使用这个参数，不需要经过任何处理。
+ * ***********************************
+ * 多个参数
+ * 任意多个参数，都会被MyBatis重新包装成一个Map传入。
+ * Map的key是[arg1, arg0, param1, param2] ，值就是参数的值。
+ * 自定义参数解决方案：
+ * 参数前面加@Param("参数名")
+ */
 
 public interface CustomerMapper {
     public Customer getCustomerWidthId(Integer id);
+    public Customer getCustomerWidthBoth(@Param("id") Integer id,@Param("name") String name);
 }
--- MyBatisProject2/src/com/gaoxing/test/MyTest.java
+++ MyBatisProject2/src/com/gaoxing/test/MyTest.java
@@ -13,5 +13,14 @@ public class MyTest {
         CustomerMapper mapper = sqlSession.getMapper(CustomerMapper.class);
         Customer customerWidthId = mapper.getCustomerWidthId(1);
         System.out.println(customerWidthId); // Customer(cust_id=1, cust_name=高星, cust_profession=射手, cust_phone=15937067033, email=12341241@qq.com)
+        sqlSession.close();
+    }
+    @Test
+    public void test2(){
+        SqlSession sqlSession = MybatisUtils.opensession();
+        CustomerMapper mapper = sqlSession.getMapper(CustomerMapper.class);
+        Customer customerWidthBoth = mapper.getCustomerWidthBoth(1, "高星");
+        System.out.println(customerWidthBoth);
+        sqlSession.close();
     }
 }
