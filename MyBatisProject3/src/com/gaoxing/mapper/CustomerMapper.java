90c672ea2750c047c90ec4a198192b374812dd4b
xinggevip <1511844263@qq.com>
2019-10-26 12:03:29 +0800

    MyBatis-查询输出Map类型

--- MyBatisProject3/src/com/gaoxing/mapper/CustomerMapper.java
+++ MyBatisProject3/src/com/gaoxing/mapper/CustomerMapper.java
@@ -1,5 +1,26 @@
 package com.gaoxing.mapper;
 
+import com.gaoxing.damain.Customer;
+import org.apache.ibatis.annotations.MapKey;
+
+import java.util.List;
+import java.util.Map;
+
 public interface CustomerMapper {
+    /* 查询总数 */
     public Integer getAccountCustomer();
+
+    /* 根据id查询客户 返回对应类 */
+    public Customer getCustomer(Integer id);
+
+    /* 根据id查询客户 返回Map类型 */
+    public Map<String,Object> getCustomerWidthId(Integer id);
+
+    /* 获取所有客户 指定字段作为key */
+    @MapKey("cust_id")
+    public Map<Integer,Customer> getAllCustomer();
+
+    /* 获取所有用户 返回list集合 */
+    public List<Customer> getAllListCustomer();
+
 }
--- MyBatisProject3/src/com/gaoxing/test/MyTest.java
+++ MyBatisProject3/src/com/gaoxing/test/MyTest.java
@@ -1,18 +1,73 @@
 package com.gaoxing.test;
 
+import com.gaoxing.damain.Customer;
 import com.gaoxing.mapper.CustomerMapper;
 import com.gaoxing.util.MybatisUtils;
 import org.apache.ibatis.session.SqlSession;
 import org.junit.Test;
 
+import java.util.List;
+import java.util.Map;
+
 public class MyTest {
     @Test
     public void getAccountCustomer(){
+        /* 查总数 */
         SqlSession sqlSession = MybatisUtils.opensession();
         CustomerMapper mapper = sqlSession.getMapper(CustomerMapper.class);
         Integer accountCustomer = mapper.getAccountCustomer();
         System.out.println(accountCustomer);
-        sqlSession.close();
+        sqlSession.close(); // 11
 
+    }
+
+    @Test
+    public void getCustomer(){
+        /* 查记录 返回对应类 */
+        SqlSession sqlSession = MybatisUtils.opensession();
+        CustomerMapper mapper = sqlSession.getMapper(CustomerMapper.class);
+        Customer customer = mapper.getCustomer(1);
+        System.out.println(customer); // Customer(cust_id=1, cust_name=高星, cust_profession=射手, cust_phone=15937067033, email=12341241@qq.com)
+        sqlSession.close();
+    }
+
+    @Test
+    public void getCustomerWidthId(){
+        /* 查记录 返回对应Map */
+        SqlSession sqlSession = MybatisUtils.opensession();
+        CustomerMapper mapper = sqlSession.getMapper(CustomerMapper.class);
+        Map<String, Object> customerWidthId = mapper.getCustomerWidthId(1);
+        System.out.println(customerWidthId); // {cust_profession=射手, cust_name=高星, cust_id=1, cust_phone=15937067033, email=12341241@qq.com}
+        sqlSession.close();
+    }
+
+    @Test
+    public void getAllCustomer(){
+        /* 查询所有客户 key为指定字段，value为对应类 */
+        SqlSession sqlSession = MybatisUtils.opensession();
+        CustomerMapper mapper = sqlSession.getMapper(CustomerMapper.class);
+        Map<Integer, Customer> allCustomer = mapper.getAllCustomer();
+        System.out.println(allCustomer);
+        // {1={cust_profession=射手, cust_name=高星, cust_id=1, cust_phone=15937067033, email=12341241@qq.com}, 2={cust_profession=刺客, cust_name=李白, cust_id=2, cust_phone=18977665521, email=libai@163.com}...}
+        sqlSession.close();
+    }
+
+    @Test
+    public void getAllListCustomer(){
+        /* 查询所有客户 返回list 集合 */
+        SqlSession sqlSession = MybatisUtils.opensession();
+        CustomerMapper mapper = sqlSession.getMapper(CustomerMapper.class);
+        List<Customer> allListCustomer = mapper.getAllListCustomer();
+        System.out.println(allListCustomer);
+        // [Customer(cust_id=1, cust_name=高星, cust_profession=射手, cust_phone=15937067033, email=12341241@qq.com), Customer(cust_id=2, cust_name=李白, cust_profession=刺客, cust_phone=18977665521, email=libai@163.com)...]
+        for (Customer customer : allListCustomer) {
+            System.out.println(customer);
+            /**
+             * Customer(cust_id=1, cust_name=高星, cust_profession=射手, cust_phone=15937067033, email=12341241@qq.com)
+             * Customer(cust_id=2, cust_name=李白, cust_profession=刺客, cust_phone=18977665521, email=libai@163.com)
+             * ...
+             */
+        }
+        sqlSession.close();
     }
 }
