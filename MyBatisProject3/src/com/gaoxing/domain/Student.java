baa1c3428ca5dd0361ce6ba26789481e18a384b0
xinggevip <1511844263@qq.com>
2019-10-27 11:28:15 +0800

    MyBatis-多对多关系表建立

---
+++ MyBatisProject3/src/com/gaoxing/domain/Student.java
@@ -0,0 +1,13 @@
+package com.gaoxing.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class Student {
+    private Integer stu_id;
+    private String stu_name;
+}
---
+++ MyBatisProject3/src/com/gaoxing/domain/Teacher.java
@@ -0,0 +1,13 @@
+package com.gaoxing.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class Teacher {
+    private Integer teacher_id;
+    private String teacher_name;
+}
